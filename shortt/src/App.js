b056345ad1cee8717d581c506220169b51e3284c
Tayyab <tayyabilyas963@gmail.com>
2023-05-09 11:34:38 +0530

    setting up

---
+++ shortt/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Shortt</h1>
+    <input type="text"/>
+    <p>Enter a URL</p>
+    <button>Shorten</button>
+    </div>
+  );
+}
+
+export default App;
