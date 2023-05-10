a823d4790077c1f6f4aa98fb1a889bb1cb9710cc
Tayyab <tayyabilyas963@gmail.com>
2023-05-10 20:21:07 +0530

    pattern&wave

--- src/App.js
+++ src/App.js
@@ -25,23 +25,26 @@ function App() {
         </nav>
         </div>
       
-      
-        <div className='font-[poppins] p-4'>
-        <div className='flex justify-center'>
-    <p className='mt-3 text-lg'>Enter a URL</p>
+      <div className='flex justify-center'>
+        <div className=''>
+          
+        <div className='font-[roboto] flex justify-center'>
+    <p className='mt-3 text-2xl'>Enter a URL</p>
     </div>
-          <div className='flex justify-center'>
+          <div className='font-[poppins] flex justify-center'>
     <input type="text" className='border border-gray-500 mt-3 rounded-lg'/>
     </div>
     
-    <div className='flex justify-center'>
+    <div className='flex font-[poppins] justify-center'>
     <button className='bg-blue-700 hover:bg-[#0099ff] text-white px-4 py-2 rounded-3xl mt-3'>Shorten</button>
     </div>
     </div>
+    </div>
+    
   
     <footer className=''>
       <div className=''>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2962ff" fill-opacity="1" d="M0,128L48,117.3C96,107,192,85,288,80C384,75,480,85,576,128C672,171,768,245,864,277.3C960,309,1056,299,1152,256C1248,213,1344,139,1392,101.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2962ff" fill-opacity="1" d="M0,160L60,170.7C120,181,240,203,360,186.7C480,171,600,117,720,128C840,139,960,213,1080,229.3C1200,245,1320,203,1380,181.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
       </div>
 
     </footer>
