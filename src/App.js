5215c54856562458e56779935622a9b437b16a66
Tayyab <tayyabilyas963@gmail.com>
2023-05-11 20:27:00 +0530

    finishing ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons';
 
 function App() {
   return (
@@ -29,10 +30,10 @@ function App() {
         <div className=''>
           
         <div className='font-[roboto] flex justify-center'>
-    <p className='mt-3 text-2xl'>Enter a URL</p>
+    <p className='mt-3  text-4xl'>Enter a URL</p>
     </div>
           <div className='font-[poppins] flex justify-center'>
-    <input type="text" className='border border-gray-500 mt-3 rounded-lg'/>
+    <input type="text" className='border border-gray-500 mt-3 rounded-lg p-1 w-[30vw]'/>
     </div>
     
     <div className='flex font-[poppins] justify-center'>
@@ -42,13 +43,30 @@ function App() {
     </div>
     
   
-    <footer className=''>
-      <div className=''>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2962ff" fill-opacity="1" d="M0,160L60,170.7C120,181,240,203,360,186.7C480,171,600,117,720,128C840,139,960,213,1080,229.3C1200,245,1320,203,1380,181.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-      </div>
+    
+      <div className='bg-blue-700 p-10'>
+        
+                
+          <div className='flex justify-center'>
+            <span className='flex font-[poppins] text-white text-sm'>
+            ©Tayyab Ilyas
+            </span>
+            </div>
+            <div className='mt-2 flex justify-center space-x-3'>
+            <div className=''>
+           <a href='https://github.com/tayyab-ilyas'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#ffffff" className='hover:fill-slate-400'><path fill-rule="evenodd" clip-rule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"></path></svg></a>
+            </div>
+            <div className=''>
+          <a href='https://www.linkedin.com/in/tayyab-ilyas/'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#ffffff" className='hover:fill-slate-400'><circle cx="4.983" cy="5.009" r="2.188"></circle><path d="M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118 1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66H9.237zm-6.142 0H6.87v12.139H3.095z"></path></svg></a>
+          </div>
+          </div>
+            </div>
+            </div>
+          
 
-    </footer>
-    </div>
+      
+
+
    
   );
 }
